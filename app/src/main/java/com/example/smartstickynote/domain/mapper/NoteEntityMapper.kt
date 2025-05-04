b0d3c347700198881156011c963c5f66c62b9109
trucphuong639 <2251120235@ut.edu.vn>
2025-05-04 23:12:43 +0700

    Delete app directory

--- app/src/main/java/com/example/smartstickynote/domain/mapper/NoteEntityMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.smartstickynote.domain.mapper
-
-import com.example.smartstickynote.data.local.entity.NoteEntity
-import com.example.smartstickynote.domain.model.Note
-
-fun NoteEntity.toDomain(): Note = Note(id, title, content, priorityRate, isFavorite, isPin, createdAt)
-fun Note.toEntity(): NoteEntity = NoteEntity(id, title, content, priorityRate, isFavorite, isPin, createdAt)
